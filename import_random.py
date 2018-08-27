d03d1f2cf0c6904b6889b321c97218bf40f331c6
Zhenyu Xu <zhenyuxu112@gmail.com>
2018-08-26 20:57:09 -0700

    新增计数功能

--- import_random.py
+++ import_random.py
@@ -1,13 +1,16 @@
 import random
 i = random.randint(1, 100)
+k = 0
 while True:
+	k += 1
 	answer = input('please guess the number: ')
 	answer = int(answer)
 	if answer == i:
-		print('congrats!!')
+		print('congrats!! you use', k, 'chances')
 		break
 	elif answer > i:
 		print('its high')
 	elif answer < i:
 		print('its low')
+	print('you use', k, 'chance')
 		
